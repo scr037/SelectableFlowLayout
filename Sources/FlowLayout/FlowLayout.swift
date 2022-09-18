ccab58ba1f366e36e82a0b2fb35008fadfa71c25
scr037 <lchinigioli@gmail.com>
2022-09-18 14:30:27 -0300

    Improving height calculation

--- Sources/FlowLayout/FlowLayout.swift
+++ Sources/FlowLayout/FlowLayout.swift
@@ -6,11 +6,18 @@ public struct FlowLayout<T: Hashable, Content: View>: View {
   private let items: [T]
   @ViewBuilder private let content: (T) -> Content
 
+  private var animation: Animation
+  private var scheduler: DispatchQueue
+
   public init(
     items: [T],
+    animation: Animation = .easeIn(duration: 0.2),
+    scheduler: DispatchQueue = DispatchQueue.main,
     @ViewBuilder content: @escaping (T) -> Content
   ) {
     self.items = items
+    self.animation = animation
+    self.scheduler = scheduler
     self.content = content
   }
 
@@ -18,8 +25,8 @@ public struct FlowLayout<T: Hashable, Content: View>: View {
     VStack {
       GeometryReader { geometry in
         self.flowContent(in: geometry)
-          .animation(.easeIn(duration: 0.2), value: items)
-          .readHeight($totalHeight, proxy: geometry)
+          .animation(animation, value: items)
+          .background(withHeight($totalHeight, scheduler: scheduler))
       }
     }
     .frame(height: totalHeight)
@@ -31,16 +38,19 @@ public struct FlowLayout<T: Hashable, Content: View>: View {
     return ZStack(alignment: .topLeading) {
       ForEach(self.items, id: \.self) { item in
         self.content(item)
-          .padding([.horizontal, .vertical], 4)
+          .padding(4)
           .alignmentGuide(.leading, computeValue: { dimensions in
             if (abs(width - dimensions.width) > geometry.size.width) {
-              width = 0
+              // End of line
               height -= dimensions.height
+              width = 0
             }
             let result = width
             if item == self.items.last {
+              // If last item, reset width..
               width = 0
             } else {
+              // Calculate new width value.
               width -= dimensions.width
             }
             return result
@@ -48,6 +58,7 @@ public struct FlowLayout<T: Hashable, Content: View>: View {
           .alignmentGuide(.top, computeValue: { _ in
             let result = height
             if item == self.items.last {
+              // If last item, reset height.
               height = 0
             }
             return result
@@ -55,14 +66,14 @@ public struct FlowLayout<T: Hashable, Content: View>: View {
         }
       }
   }
-}
 
-extension View {
-  fileprivate func readHeight(_ totalHeight: Binding<CGFloat>, proxy: GeometryProxy) -> some View {
-    DispatchQueue.main.async {
-      totalHeight.wrappedValue = proxy.frame(in: .local).size.height
+  private func withHeight(_ binding: Binding<CGFloat>, scheduler: DispatchQueue) -> some View {
+    return GeometryReader { proxy -> Color in
+      scheduler.async {
+        binding.wrappedValue = proxy.frame(in: .local).size.height
+      }
+      return .clear
     }
-    return self
   }
 }
 
